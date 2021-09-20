523ac20c4cb159ab2f603a72696a1668bc280350
Thomas Witherow <twitherow@bellsouth.net>
2021-09-20 13:54:29 -0500

    initial

---
+++ DogGo/Repositories/IWalkerRepository.cs
@@ -0,0 +1,19 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public interface IWalkerRepository
+    {
+        List<Walker> GetAllWalkers();
+        Walker GetWalkerById(int id);
+
+       
+     
+
+
+    }
+
+
+}
